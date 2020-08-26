5c9c5ff8b5c8981615a337059664c67ad15f6a39
Paul H Bartley <paul.h.bartley@gmail.com>
2020-08-25 21:13:04 -0400

    updated ckvp

--- collect_kv_pairs.py
+++ collect_kv_pairs.py
@@ -1,9 +1,11 @@
-# Form a single dictionary of collected key:value pairs from a list of dictionaries
+# Collect values from a list of dictionaries as the key:value pairs within one single dictionary.
 
 list_of_dicts = [{'A': 1, 'B': 2}, 
                  {'A': 3, 'B': 4},
-                 {'A': 5, 'B': 6}]
+                 {'A': 5, 'B': 6},
+                 {'A': 7, 'B': 8},
+                 {'A': 9, 'B': 10}]
 
-collected_kv_pairs = {value['A']:value['B'] for value in list_of_dicts}
+collected_kv_pairs = {dict_value['A']:dict_value['B'] for dict_value in list_of_dicts}
 
 print(collected_kv_pairs)
